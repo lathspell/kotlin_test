59798fc4c91bdd595218f9a0e50ac85efad81d56
Christian Brunotte <christian.brunotte@obi.de>
2024-03-19 15:05:04 +0100

    Deleted broken tests

--- springboot-jpa/src/main/kotlin/de/lathspell/test/jpa/a1/PersonRepo.kt
+++
@@ -1,8 +0,0 @@
-package de.lathspell.test.jpa.a1
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import java.util.*
-
-@Repository
-interface PersonRepo : JpaRepository<Person, UUID>
