83571dc5bb51d03fb4dcc9386a2fbea28bf44f88
Christian Brunotte <christian.brunotte@obi.de>
2025-01-24 17:39:50 +0100

    updates

--- springboot-jpa/build.gradle.kts
+++ springboot-jpa/build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.9.23"
+    val kotlinVersion = "2.1.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("plugin.jpa") version kotlinVersion
 
-    id("com.github.ben-manes.versions") version "0.51.0"        // https://github.com/ben-manes/gradle-versions-plugin for ":dependencyUpdates"
-    id("io.spring.dependency-management") version "1.1.0"
-    id("org.springframework.boot") version "3.2.3"
+    id("com.github.ben-manes.versions") version "0.52.0"        // https://github.com/ben-manes/gradle-versions-plugin for ":dependencyUpdates"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.springframework.boot") version "3.4.2"
 }
 
 group = "de.lathspell.test"
@@ -20,12 +20,15 @@ repositories {
 dependencies {
     // JSON
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // SQL
+    testRuntimeOnly("com.h2database:h2")
+    testRuntimeOnly("org.postgresql:postgresql:42.7.5")
+    val flywayVersion = "10.20.1"
+    runtimeOnly("org.flywaydb:flyway-core:$flywayVersion")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql:$flywayVersion")
+    testImplementation("org.flywaydb.flyway-test-extensions:flyway-spring-test:10.0.0")
     // JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    testRuntimeOnly("com.h2database:h2")
-    testRuntimeOnly("org.postgresql:postgresql:42.7.3")
-    implementation("org.flywaydb:flyway-core:9.22.3")
-    testImplementation("org.flywaydb.flyway-test-extensions:flyway-spring-test:9.5.0")
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
@@ -36,8 +39,8 @@ kotlin {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
+    compilerOptions {
+        freeCompilerArgs.add("-Xjsr305=strict")
     }
 }
 
